aed57afd44934739e1e6a32652f39a361e1093e5
j b <j.is.brown@gmail.com>
2024-08-06 18:54:52 -0500

    finished index.js, plastic.js, and discs.js in model

--- models/index.js
+++ models/index.js
@@ -1,10 +1,43 @@
 const User = require('./User');
 const Category = require("./category");
 const Discs = require("./discs");
+const Plastic = require("./plastic");
+const UserDiscs = require('./UserDiscs');
+
+User.belongsToMany(Discs, {
+    through: UserDiscs,
+    foreignKey: 'user_id'
+});
+
+
+Discs.belongsToMany(User, {
+    through: UserDiscs,
+    foreignKey: 'disc_id'
+});
+
+Discs.belongsTo(Category, {
+    foreignKey: 'category_id'
+})
+
+Category.hasMany(Discs, {
+    foreignKey: 'category_id'
+});
+
+Discs.belongsTo(Plastic, {
+    foreignKey: 'plastic_id'
+})
+
+Plastic.hasMany(Discs, {
+    foreignKey: 'plastic_id'
+});
+
+
 
 module.exports = {
     User,
     Category,
     Discs,
+    Plastic,
+    UserDiscs
 };
 
