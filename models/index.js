1c2228927f83bf79b8849202d47626ea0adcfd67
j b <j.is.brown@gmail.com>
2024-08-05 19:06:33 -0500

    index.js for models updated

--- models/index.js
+++ models/index.js
@@ -1,5 +1,18 @@
 const User = require('./User');
+const Category = require("./category");
+const DistanceDrivers = require("./distance");
+const Fairway = require("./fairway");
+const Midrange = require("./mid");
+const PuttAndApproach = require('./putt-approach');
+const Putt = require('./putt');
 
-// Write Table relationships here
+module.exports = {
+    User,
+    Category,
+    DistanceDrivers,
+    Fairway,
+    Midrange,
+    PuttAndApproach,
+    Putt
+};
 
-module.exports = { User };
