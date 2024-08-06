e7aff7f590f3fe8f3f34302e7f96f1387fe264e1
j b <j.is.brown@gmail.com>
2024-08-05 20:14:32 -0500

    file restructure and renaming

---
+++ models/discs.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Distance extends Model {}
+
+Distance.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true, 
+            autoIncrement: true,
+        }, 
+        Distance_name: {
+            type: DataTypes.STRING,
+            allowNull: false, 
+        },
+        Distance_plastic: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        Distance_color: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        category_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "category", 
+                key: "id",
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "distance",
+    },
+);
+
+module.exports = distance;
