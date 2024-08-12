e03bd17f0c8432750a57aee726c1d7c68f6f6f4a
ut463 <matthewcomer@comcast.net>
2024-08-12 18:50:18 -0500

    git ignore

--- controllers/pages/disk-page.js
+++ controllers/pages/disk-page.js
@@ -2,8 +2,14 @@ const router = require('express').Router();
 
 router.get('/', async (req, res) => {
   try {
+    // get the discs here
+    // const discs = await 
+
+    // console.log(disc);
+    // pass them in here
     res.render('disk', {
-      logged_in: true
+      logged_in: true,
+      discs
     });
   } catch (err) {
     res.status(500).json(err);
